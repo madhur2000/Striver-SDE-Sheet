42de22a2905eb8c58bce62dcd6284c5f3e8c5013
Madhur Rakheja <55992831+madhur2000@users.noreply.github.com>
2021-09-20 08:53:51 +0530

    Update #62 UniquePaths.java

--- #62 UniquePaths.java
+++ #62 UniquePaths.java
@@ -1,23 +1,38 @@
 class Solution {
     public int uniquePaths(int m, int n) {
         
+//         Approach-1: Recursion
+        
 //         if(m == 1 || n == 1)
 //             return 1;
         
 //         return (uniquePaths(m-1, n) + uniquePaths(m, n-1));
         
-        int[][] storage = new int[m+1][n+1];
+//         APproach-2: DP
         
-        for(int i=1; i<storage.length; i++){
-            for(int j=1; j<storage[0].length; j++){
-                if(i == 1 || j == 1){
-                    storage[i][j] = 1;
-                }
-                else{
-                    storage[i][j] = storage[i-1][j] + storage[i][j-1];
-                }
-            }
+//         int[][] storage = new int[m+1][n+1];
+        
+//         for(int i=1; i<storage.length; i++){
+//             for(int j=1; j<storage[0].length; j++){
+//                 if(i == 1 || j == 1){
+//                     storage[i][j] = 1;
+//                 }
+//                 else{
+//                     storage[i][j] = storage[i-1][j] + storage[i][j-1];
+//                 }
+//             }
+//         }
+//         return storage[m][n];
+        
+//         Approach-3: Combinations
+        
+        int num = m+n-2;
+        int r = m-1;
+//         Calculating (num)C(r):
+        double res = 1;
+        for(int i=1; i<=r; i++){
+            res = (res * (num-r+i))/i;
         }
-        return storage[m][n];
+        return (int)res;
     }
 }
